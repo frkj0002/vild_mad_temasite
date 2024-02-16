0ae6d0ade7120c4c5d169f8283fd717c600cd9b9
Line <lith0001@stud.kea.dk>
2024-02-16 19:40:52 +0100

    indhold og det meste styling tilføjet

---
+++ js/questions.js
@@ -0,0 +1,18 @@
+// Border rundt om det valgte svar 
+// const answer = document.querySelector(".answerBox");
+
+
+// answer.addEventListener("click", answerChosen);
+
+// function answerChosen() {
+    
+//     answer.classList.add("chosen");
+    
+// }
+
+
+// document.querySelectorAll(".answerBox").forEach((n) =>
+//   n.addEventListener("click", () => {
+//     navMenu.classList.remove("active");
+//   })
+// );
