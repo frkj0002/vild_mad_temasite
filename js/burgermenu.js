66b0920bba49471fb2a6aec179086da8f03834fc
frkj0002 <143484944+frkj0002@users.noreply.github.com>
2024-02-18 22:08:25 +0100

    header done

---
+++ js/burgermenu.js
@@ -0,0 +1,16 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+const links = document.querySelectorAll(".menu li a");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    burger.classList.remove("active");
+    nav.classList.remove("active");
+  });
+});
