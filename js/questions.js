11584417b2a4d8436dea77e6b1a53e232186e195
Line <lith0001@stud.kea.dk>
2024-02-19 12:23:35 +0100

    flere rettelser

--- js/questions.js
+++ js/questions.js
@@ -13,6 +13,25 @@
 
 // document.querySelectorAll(".answerBox").forEach((n) =>
 //   n.addEventListener("click", () => {
-//     navMenu.classList.remove("active");
+//     navMenu.classList.add("chosen");
 //   })
 // );
+
+let answers = document.querySelector(".answers");
+// answers.forEach(setColor);
+answers.forEach(chosenAnswer).addEventListener("click", setColor);
+
+
+
+
+function setColor(chosenAnswer) {
+    console.log(setColor);
+    let chosenAnswer = document.querySelectorAll(".answers a");
+    if ("click") {
+        console.log("classlist added")
+        chosenAnswer.classList.add("chosen");       
+    }
+    else {
+        // chosenAnswer.classList.remove("chosen");
+    }
+}
