65b34f183db14a0d5afab5330942a5c13a8d196e
Line <lith0001@stud.kea.dk>
2024-02-21 12:46:08 +0100

    questionsite færdig-ish

--- js/questions.js
+++ js/questions.js
@@ -1,33 +1,5 @@
-// Filtrering af data
-
 // Definerer variabler
-// const herbsUrl = "https://vuifwijcjpzjqqspgrxn.supabase.co/rest/v1/herbs?order=id.asc&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1aWZ3aWpjanB6anFxc3BncnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MDE2NTAsImV4cCI6MjAyMzQ3NzY1MH0.d0KA4WqvNlBMrmaDdam3yl6R7rhU8NevlOTM3BBzeYM"
-
-// Fetcher dataen fra url'en
-// fetch(herbsUrl)
-//     .then(res=>res.json())
-//     .then(showHerbs);
-
-// function showHerbs(herbs) {
-//     console.log("All herbs:", herbs);
-// }
-
-
-
-          
-
-const urlParams = new URLSearchParams(window.location.search);
-const colors = urlParams.get("colors");
-const herbsUrl = `https://vuifwijcjpzjqqspgrxn.supabase.co/rest/v1/herbs?&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1aWZ3aWpjanB6anFxc3BncnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MDE2NTAsImV4cCI6MjAyMzQ3NzY1MH0.d0KA4WqvNlBMrmaDdam3yl6R7rhU8NevlOTM3BBzeYM${colors}`
-
-// Fetcher dataen fra url'en
-fetch(herbsUrl)
-    .then(res=>res.json())
-    .then(showHerbs);
-
-function showHerbs(herbs) {
-    console.log(herbs);
-}
+const herbsUrl = "https://vuifwijcjpzjqqspgrxn.supabase.co/rest/v1/herbs?&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1aWZ3aWpjanB6anFxc3BncnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MDE2NTAsImV4cCI6MjAyMzQ3NzY1MH0.d0KA4WqvNlBMrmaDdam3yl6R7rhU8NevlOTM3BBzeYM"
 
 // Sæson variabler
 let spring = document.querySelector("#firstQuestion .answerBtn:nth-child(1)");
@@ -42,43 +14,85 @@ winter.addEventListener("click", resultsWinter);
 // Sæson funktioner
 function resultsSpring() {
     console.log("Spring chosen");
-    // Tilføj rødbrun baggrundsfarve til valgte svar
+    // Tilføjer rødbrun baggrundsfarve til valgte svar
     spring.classList.add("chosen");
 
-    // Fjern baggrundsfarve fra de andre svar
+    // Fjerner baggrundsfarve fra de andre svar
     summer.classList.remove("chosen");
     fall.classList.remove("chosen");
     winter.classList.remove("chosen");
+
+    // Fetcher og filtrerer den data det skal bruges
+    fetch(herbsUrl)
+    .then(response => response.json())
+    .then(data => {
+
+    const springHerbs = data.filter(herb => herb.seasons.includes('Spring'));
+
+    console.log(springHerbs);
+    })
+
 }
 function resultsSummer() {
     console.log("Summer chosen");
-    // Tilføj rødbrun baggrundsfarve til valgte svar
+    // Tilføjer rødbrun baggrundsfarve til valgte svar
     summer.classList.add("chosen");
 
-    // Fjern baggrundsfarve fra de andre svar
+    // Fjerner baggrundsfarve fra de andre svar
     spring.classList.remove("chosen");
     fall.classList.remove("chosen");
     winter.classList.remove("chosen");
+
+    // Fetcher og filtrerer den data det skal bruges
+    fetch(herbsUrl)
+    .then(response => response.json())
+    .then(data => {
+
+    const summerHerbs = data.filter(herb => herb.seasons.includes('Summer'));
+
+    console.log(summerHerbs);
+    })
+
 }
 function resultsFall() {
     console.log("Fall chosen");
-    // Tilføj rødbrun baggrundsfarve til valgte svar
+    // Tilføjer rødbrun baggrundsfarve til valgte svar
     fall.classList.add("chosen");
 
-    // Fjern baggrundsfarve fra de andre svar
+    // Fjerner baggrundsfarve fra de andre svar
     spring.classList.remove("chosen");
     summer.classList.remove("chosen");
     winter.classList.remove("chosen");
+
+    // Fetcher og filtrerer den data det skal bruges
+    fetch(herbsUrl)
+    .then(response => response.json())
+    .then(data => {
+
+    const fallHerbs = data.filter(herb => herb.seasons.includes('Fall'));
+
+    console.log(fallHerbs);
+    })
 }
 function resultsWinter() {
     console.log("Winter chosen");
-    // Tilføj rødbrun baggrundsfarve til valgte svar
+    // Tilføjer rødbrun baggrundsfarve til valgte svar
     winter.classList.add("chosen");
 
-    // Fjern baggrundsfarve fra de andre svar
+    // Fjerner baggrundsfarve fra de andre svar
     spring.classList.remove("chosen");
     summer.classList.remove("chosen");
     fall.classList.remove("chosen");
+
+    // Fetcher og filtrerer den data det skal bruges
+    fetch(herbsUrl)
+    .then(response => response.json())
+    .then(data => {
+
+    const winterHerbs = data.filter(herb => herb.seasons.includes('Winter'));
+
+    console.log(winterHerbs);
+    })
 }
 
 // Landskab variabler
@@ -94,43 +108,101 @@ fields.addEventListener("click", resultsFields);
 // Landskab funktioner
 function resultsBeach() {
     console.log("Beach chosen");
-    // Tilføj rødbrun baggrundsfarve til valgte svar
+    // Tilføjer rødbrun baggrundsfarve til valgte svar
     beach.classList.add("chosen");
 
-    // Fjern baggrundsfarve fra de andre svar
+    // Fjerner baggrundsfarve fra de andre svar
     forest.classList.remove("chosen");
     city.classList.remove("chosen");
     fields.classList.remove("chosen");
+
+    // Fetcher og filtrerer den data det skal bruges
+    fetch(herbsUrl)
+    .then(response => response.json())
+    .then(data => {
+    // De arrays der skal bruges 
+    const targetLandscapes = ['Fence', 'Roadside', 'Sea marshes', 'Beach', 'Grassland'];
+        
+    const beachHerbs = data.filter(herb => {
+
+    // Checker, hvilke urter i dataen der har de ovenstående arrays og viser urterne i konsollen
+    return targetLandscapes.some(landscape => herb.landscapes.includes(landscape));
+    });
+
+    console.log(beachHerbs);
+    })
 }
 function resultsForest() {
     console.log("Forest chosen");
-    // Tilføj rødbrun baggrundsfarve til valgte svar
+    // Tilføjer rødbrun baggrundsfarve til valgte svar
     forest.classList.add("chosen");
 
-    // Fjern baggrundsfarve fra de andre svar
+    // Fjerner baggrundsfarve fra de andre svar
     beach.classList.remove("chosen");
     city.classList.remove("chosen");
     fields.classList.remove("chosen");
+
+    // Fetcher og filtrerer den data det skal bruges
+    fetch(herbsUrl)
+    .then(response => response.json())
+    .then(data => {
+    // De arrays der skal bruges 
+    const targetLandscapes = ['Fence', 'Roadside', 'Lake', 'A sea', 'Stream', 'Coniferous forest', 'Deciduous forest'];
+        
+    const forestHerbs = data.filter(herb => {
+
+    // Checker, hvilke urter i dataen der har de ovenstående arrays og viser urterne i konsollen
+    return targetLandscapes.some(landscape => herb.landscapes.includes(landscape));
+    });
+
+    console.log(forestHerbs);
+    })
 }
 function resultsCity() {
     console.log("City chosen");
-    // Tilføj rødbrun baggrundsfarve til valgte svar
+    // Tilføjer rødbrun baggrundsfarve til valgte svar
     city.classList.add("chosen");
 
-    // Fjern baggrundsfarve fra de andre svar
+    // Fjerner baggrundsfarve fra de andre svar
     beach.classList.remove("chosen");
     forest.classList.remove("chosen");
     fields.classList.remove("chosen");
+    
+    // Fetcher og filtrerer den data det skal bruges
+    fetch(herbsUrl)
+    .then(response => response.json())
+    .then(data => {
+
+    const townHerbs = data.filter(herb => herb.landscapes.includes('Town'));
+
+    console.log(townHerbs);
+    })
 }
 function resultsFields() {
     console.log("Fields chosen");
-    // Tilføj rødbrun baggrundsfarve til valgte svar
+    // Tilføjer rødbrun baggrundsfarve til valgte svar
     fields.classList.add("chosen");
 
-    // Fjern baggrundsfarve fra de andre svar
+    // Fjerner baggrundsfarve fra de andre svar
     beach.classList.remove("chosen");
     forest.classList.remove("chosen");
     city.classList.remove("chosen");
+
+    // Fetcher og filtrerer den data det skal bruges
+    fetch(herbsUrl)
+    .then(response => response.json())
+    .then(data => {
+    // De arrays der skal bruges 
+    const targetLandscapes = ['Grassland', 'Fence', 'Roadside', 'Salt marshes'];
+        
+    const fieldsHerbs = data.filter(herb => {
+
+    // Checker, hvilke urter i dataen der har de ovenstående arrays og viser urterne i konsollen
+    return targetLandscapes.some(landscape => herb.landscapes.includes(landscape));
+    });
+
+    console.log(fieldsHerbs);
+    })
 }
 
 // Farve variabler
@@ -146,70 +218,89 @@ purple.addEventListener("click", resultsPurple);
 // Farve funktioner
 function resultsWhite() {
     console.log("White chosen");
-    // Tilføj rødbrun baggrundsfarve til valgte svar
+    // Tilføjer rødbrun baggrundsfarve til valgte svar
     white.classList.add("chosen");
 
-    // Fjern baggrundsfarve fra de andre svar
+    // Fjerner baggrundsfarve fra de andre svar
     yellow.classList.remove("chosen");
     green.classList.remove("chosen");
     purple.classList.remove("chosen");
+
+    // Fetcher og filtrerer den data det skal bruges
+    fetch(herbsUrl)
+    .then(response => response.json())
+    .then(data => {
+
+    const whiteHerbs = data.filter(herb => herb.colors.includes('White'));
+
+    console.log(whiteHerbs);
+    })
 }
 function resultsYellow() {
     console.log("Yellow chosen");
-    // Tilføj rødbrun baggrundsfarve til valgte svar
+    // Tilføjer rødbrun baggrundsfarve til valgte svar
     yellow.classList.add("chosen");
 
-    // Fjern baggrundsfarve fra de andre svar
+    // Fjerner baggrundsfarve fra de andre svar
     white.classList.remove("chosen");
     green.classList.remove("chosen");
     purple.classList.remove("chosen");
+
+    // Fetcher og filtrerer den data det skal bruges
+    fetch(herbsUrl)
+    .then(response => response.json())
+    .then(data => {
+
+    const yellowHerbs = data.filter(herb => herb.colors.includes('Yellow'));
+
+    console.log(yellowHerbs);
+    })
 }
 function resultsGreen() {
     console.log("Green chosen");
-    // Tilføj rødbrun baggrundsfarve til valgte svar
+    // Tilføjer rødbrun baggrundsfarve til valgte svar
     green.classList.add("chosen");
 
-    // Fjern baggrundsfarve fra de andre svar
+    // Fjerner baggrundsfarve fra de andre svar
     white.classList.remove("chosen");
     yellow.classList.remove("chosen");
     purple.classList.remove("chosen");
+
+    // Fetcher og filtrerer den data det skal bruges
+    fetch(herbsUrl)
+    .then(response => response.json())
+    .then(data => {
+
+    const greenHerbs = data.filter(herb => herb.colors.includes('Green'));
+
+    console.log(greenHerbs);
+    })
 }
 function resultsPurple() {
     console.log("Purple chosen");
-    // Tilføj rødbrun baggrundsfarve til valgte svar
+    // Tilføjer rødbrun baggrundsfarve til valgte svar
     purple.classList.add("chosen");
 
-    // Fjern baggrundsfarve fra de andre svar
+    // Fjerner baggrundsfarve fra de andre svar
     white.classList.remove("chosen");
     yellow.classList.remove("chosen");
     green.classList.remove("chosen");
 
-    
-    // Definerer variabler
-    // const urlParams = new URLSearchParams(window.location.search);
-    // const purple = urlParams.get("purple");
-    // const herbsUrl = `https://vuifwijcjpzjqqspgrxn.supabase.co/rest/v1/herbs/${colors["Purple"]}?order=id.asc&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1aWZ3aWpjanB6anFxc3BncnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MDE2NTAsImV4cCI6MjAyMzQ3NzY1MH0.d0KA4WqvNlBMrmaDdam3yl6R7rhU8NevlOTM3BBzeYM`
+    // Fetcher og filtrerer den data det skal bruges
+    fetch(herbsUrl)
+    .then(response => response.json())
+    .then(data => {
 
-    // Fetcher dataen fra url'en
-    // fetch(herbsUrl)
-    //     .then(res=>res.json())
-    //     .then(showPurple);
-        
-    // function showPurple(purple) {
-    //     console.log("Herbs with purple", herbs);
-    // }
+    const purpleHerbs = data.filter(herb => herb.colors.includes('Purple'));
+
+    console.log(purpleHerbs);
+    })
+
 }
 
+let confirmBtn = document.querySelector(".resultBtn");
+confirmBtn.querySelector("a").href = `list.html?`
 
-
-let confirmBtn = document.querySelector("#resultBtn");
-confirmBtn.addEventListener("click", seeResults);
-
-function seeResults(result) {
-    console.log("Results");
-
-    document.querySelector(".breadcrumbs li:nth-child(3) a").href = `productlist.html?category=${product.category}`
-}
 
 
 
