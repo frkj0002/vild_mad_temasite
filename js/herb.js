34dad367eafbd4af20196c50b373c52dc0d5aea3
Line <lith0001@stud.kea.dk>
2024-02-21 12:52:05 +0100

    first commit

--- js/herb.js
+++ js/herb.js
@@ -1,14 +1,7 @@
-// https://vuifwijcjpzjqqspgrxn.supabase.co
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1aWZ3aWpjanB6anFxc3BncnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MDE2NTAsImV4cCI6MjAyMzQ3NzY1MH0.d0KA4WqvNlBMrmaDdam3yl6R7rhU8NevlOTM3BBzeYM
-
-// window.addEventListener("DOMContentLoaded", showProduct);
-
 const urlParams = new URLSearchParams(window.location.search);
 // console.log("urlParams", urlParams);
 var id = urlParams.get("id");
 if (id == undefined) id = 1;
-// const id = 343;
-// const url = `https://vuifwijcjpzjqqspgrxn.supabase.co/rest/v1/herbs?id=${id}&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1aWZ3aWpjanB6anFxc3BncnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MDE2NTAsImV4cCI6MjAyMzQ3NzY1MH0.d0KA4WqvNlBMrmaDdam3yl6R7rhU8NevlOTM3BBzeYM`;
 
 fetch("https://vuifwijcjpzjqqspgrxn.supabase.co/rest/v1/herbs?id=eq." + id, {
   method: "GET",
@@ -24,49 +17,6 @@ function showSingleHerb(product) {
   document.querySelector("h1").textContent = product.name;
 }
 
-// showSingleHerb();
-// , {
-//     method: "GET",
-//     headers: {
-//       apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1aWZ3aWpjanB6anFxc3BncnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MDE2NTAsImV4cCI6MjAyMzQ3NzY1MH0.d0KA4WqvNlBMrmaDdam3yl6R7rhU8NevlOTM3BBzeYM",
-//     },
-//   }
-
-// function showProduct() {
-//   console.log("hej");
-//   fetch(url)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (json) {
-//       visProdukt(json);
-//     });
-// }
-
-// function visProdukt(produkt) {
-//   console.log(produkt);
-//   document.querySelector("h1").textContent = produkt.name;
-//   // document.querySelector(".product_img").src = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
-//   // document.querySelector(".underkategori").textContent = produkt.subcategory;
-//   // document.querySelector(".maerke").textContent = produkt.brandname;
-//   // document.querySelector(".pris").textContent = "DKK " + produkt.price + " ,-";
-
-//   // if (produkt.discount) {
-//   //   document.querySelector(".udsalg_procent").classList.remove("skjul");
-//   //   document.querySelector(".udsalg_pris_før").classList.remove("skjul");
-//   //   document.querySelector(".udsalg_pris_før").classList.add("pris_før");
-//   //   document.querySelector("h4").classList.add("udsalg_pris_efter");
-//   //   document.querySelector("h4").textContent = "DKK " + Math.round((produkt.price * (100 - produkt.discount)) / 100) + " ,-";
-//   // }
-//   // document.querySelector("#procent").textContent = produkt.discount + "%";
-
-//   // if (produkt.soldout) {
-//   //   document.querySelector(".udsolgt_tekst").classList.remove("skjul");
-//   //   document.querySelector(".product_img").classList.add("udsolgt");
-//   // }
-// }
-
-// showProduct();
 
 // SLIDESHOW
 
